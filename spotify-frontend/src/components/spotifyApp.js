b8c813674f125bb86ead935391f19f94ffd56a34
Liam McColley <athenasbump@gmail.com>
2022-05-31 14:13:29 -0400

    discover lowercase

--- spotify-frontend/src/components/spotifyApp.js
+++ spotify-frontend/src/components/spotifyApp.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import DiscoverPage from "./DiscoverPage";
+import DiscoverPage from "./discoverPage";
 import LoginPage from "./loginPage";
 
 function SpotifyApp() {
