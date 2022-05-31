fc2db04bf08d837d0681e8c58f4207af5f7c828e
avb5bt <81479081+avb5bt@users.noreply.github.com>
2022-05-31 13:41:41 -0400

    get request for users collection

--- spotify-backend/routes/users.js
+++ spotify-backend/routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const db = require("./firebase")
+const {getDocs, collection, addDoc} = require("firebase/firestore")
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+router.get("/data", async (req, res, next) => {
+  const allDocData = []
+  // console.log(req.query)  // shows the URL params (stuff after the ? in the URL)
+  const docs = await getDocs(collection(db, "users"))
+  docs.forEach((doc) => allDocData.push(doc.data()))
+  res.json({result: allDocData})
+})
 
 module.exports = router;
