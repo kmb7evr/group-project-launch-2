1f5bc6fea569246b9656cda56ebc4a71aeccef63
Liam McColley <athenasbump@gmail.com>
2022-06-02 14:06:54 -0400

    fixed an import

--- spotify-frontend/src/components/Conversation.js
+++ spotify-frontend/src/components/Conversation.js
@@ -8,22 +8,22 @@ import { useLocation } from 'react-router-dom';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import IndivForum from "./IndivForum.js";
-import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
 
 function Conversation() {
     const location = useLocation();
-    const contact=location.state?.contact
-    const userName=location.state?.userName
+    const contact = location.state?.contact
+    const userName = location.state?.userName
     const messageRef = useRef(null);
-    const [conversation, setConversation]=useState()
+    const [conversation, setConversation] = useState()
 
 
     useEffect(() => {
         fetch("http://localhost:9000/inbox/indivConversation?name=" + userName + "?" + contact)
-        .then((res) => res.json())
-        .then((text) => setConversation(text.result))
-        .catch((err) => console.log(err))
-      }, [])
+            .then((res) => res.json())
+            .then((text) => setConversation(text.result))
+            .catch((err) => console.log(err))
+    }, [])
 
 
     const sendMessage = (e) => {
@@ -35,37 +35,37 @@ function Conversation() {
             Message: messageRef.current.value
         })
 
-        .then((res) => console.log(res.data))
-        .catch((err) => console.log(err))
+            .then((res) => console.log(res.data))
+            .catch((err) => console.log(err))
 
-        messageRef.current.value=""
+        messageRef.current.value = ""
         window.location.reload(false);
     }
-    
-  return (
-    <center>
-    <div className="IndivConversation">
-        <Link to="/Inbox">Return to Inbox</Link> 
-        <h1>Conversation with: {contact}</h1>
-        <form onSubmit={sendMessage} >
-                New Message:
-                <textarea type="text" ref={messageRef} rows="4" cols="50"/>
-                <input type="submit" value="Send Message"/>
-        </form>
 
-        {conversation && conversation.map((c, index) => 
-            {return c.sent ?
-                <div>
-                    <p>Sent: {c.message}</p>
-                </div>:
-                <div>
-                    <p>Recieved: {c.message}</p> 
-                </div>
-            }
-        )}
-    </div>
-    </center>
-  );
+    return (
+        <center>
+            <div className="IndivConversation">
+                <Link to="/Inbox">Return to Inbox</Link>
+                <h1>Conversation with: {contact}</h1>
+                <form onSubmit={sendMessage} >
+                    New Message:
+                    <textarea type="text" ref={messageRef} rows="4" cols="50" />
+                    <input type="submit" value="Send Message" />
+                </form>
+
+                {conversation && conversation.map((c, index) => {
+                    return c.sent ?
+                        <div>
+                            <p>Sent: {c.message}</p>
+                        </div> :
+                        <div>
+                            <p>Recieved: {c.message}</p>
+                        </div>
+                }
+                )}
+            </div>
+        </center>
+    );
 }
 
 export default Conversation;
